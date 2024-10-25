05179de885db20f64971170d8338d063d7e3fb2d
Travis Hoki <travis.hoki@gmail.com>
2024-10-25 01:29:55 -0600

    spot location

--- src/TattooCanvas/Spot.tsx
+++ src/TattooCanvas/Spot.tsx
@@ -7,11 +7,13 @@ import { firstPath } from "./Spot.helpers";
 export const Spot = ({ link, title, x, y }: SpotProps) => {
   const location = useLocation();
   const isActive = firstPath(link) === firstPath(location.pathname);
+  const left = x;
+  const top = y;
 
   return (
     <Link
       className={classnames("spot", { active: isActive })}
-      style={{ left: x, top: y }}
+      style={{ left, top }}
       title={title}
       to={link}
     />
