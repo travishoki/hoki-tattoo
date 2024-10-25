be0f8815283bddb2947a575229735bd99532a935
Travis Hoki <travis.hoki@gmail.com>
2024-10-25 15:20:40 -0600

    color

--- src/pages/DragonColorPage.tsx
+++ src/pages/DragonColorPage.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Page } from '../components/Page/Page';
 import { ExternalLink } from '../components/ExternalLink/ExternalLink';
+import './DragonColorPage.scss';
 
 export const DragonColorPage = () => {
 	return (
@@ -14,37 +15,40 @@ export const DragonColorPage = () => {
 				</li>
 
 				<li>
-					Yellow is superior, imperial, a symbol of the centre of the Earth,
-					linked to the spleen and stomach. A yellow dragon robe was reserved
-					for the Emperor (the Son of Heaven) and his family alone.
+					<span className="color yellow">Yellow</span> is superior, imperial, a
+					symbol of the centre of the Earth, linked to the spleen and stomach. A
+					yellow dragon robe was reserved for the Emperor (the Son of Heaven)
+					and his family alone.
 				</li>
 
 				<li>
-					Gold coloured dragons are also special. They have special attributes
-					such as wealth, wisdom, kindness and the ability to face challenges
-					head on.
+					<span className="color gold">Gold</span> coloured dragons are also
+					special. They have special attributes such as wealth, wisdom, kindness
+					and the ability to face challenges head on.
 				</li>
 
 				<li>
-					Blue and Green are symbols of the East, the chief spirits of the
-					Spring. The blues are pure azure. Green represents good fortune, good
-					health, or luck.
+					<span className="color blue">Blue</span> and{' '}
+					<span className="color green">Green</span> are symbols of the East,
+					the chief spirits of the Spring. The blues are pure azure. Green
+					represents good fortune, good health, or luck.
 				</li>
 
 				<li>
-					Red is associated with storms, the South, Summer, the heart and large
-					intestine, fire, passion, activity, and good fortune.
+					<span className="color red">Red</span> is associated with storms, the
+					South, Summer, the heart and large intestine, fire, passion, activity,
+					and good fortune.
 				</li>
 
 				<li>
-					Black is associated with storms, the North, winter, vengeance, and
-					worry.
+					<span className="color black">Black</span> is associated with storms,
+					the North, winter, vengeance, and worry.
 				</li>
 
 				<li>
-					White represents the West, Autumn, mourning, and death. To my own
-					knowledge, it&apos;s different from the association of the color white
-					with purity.
+					<span className="color white">White</span> represents the West,
+					Autumn, mourning, and death. To my own knowledge, it&apos;s different
+					from the association of the color white with purity.
 				</li>
 			</ul>
 
