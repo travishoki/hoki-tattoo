fd448e2b169f47a2f1c0df6d3eea6e101fe805c4
Travis Hoki <travis.hoki@gmail.com>
2024-10-25 14:23:53 -0600

    spot list

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import { Page } from '../components/Page/Page';
+import { SubPageLinks } from '~components/SubPageLinks/SubPageLinks';
+import { SPOTS } from 'src/const';
+
+const links = SPOTS;
 
 export const HomePage = () => {
 	return (
 		<Page title="Pick A Spot">
 			<p>Click a circle to learn more</p>
+			<SubPageLinks links={links} />
 		</Page>
 	);
 };
