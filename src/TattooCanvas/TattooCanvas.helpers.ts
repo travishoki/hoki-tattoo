a5054d938e9a408c29b73828f30d6755df86b5cb
Travis Hoki <travis.hoki@gmail.com>
2024-11-05 19:11:39 -0700

    dimension helper

---
+++ src/TattooCanvas/TattooCanvas.helpers.ts
@@ -0,0 +1,40 @@
+import { LAPTOP_HEIGHT, ORIGINAL_HEIGHT, ORIGINAL_WIDTH } from 'src/const';
+import { getHeightFromWidth, getWidthFromHeight } from '~helpers/numbers';
+import { getIsPortrait, getScreenWidth } from '~helpers/screen';
+
+const isPortrait = getIsPortrait();
+
+export const getCanvasDimensions = (
+	viewerWidth: number,
+	viewerHeight: number,
+) => {
+	let width: number;
+	let height = LAPTOP_HEIGHT;
+	let left = 0;
+	let top = 0;
+
+	if (isPortrait) {
+		const shouldUseHeight =
+			viewerWidth / viewerHeight > ORIGINAL_WIDTH / ORIGINAL_HEIGHT;
+
+		if (shouldUseHeight) {
+			height = viewerHeight;
+			width = getWidthFromHeight(height, ORIGINAL_WIDTH, ORIGINAL_HEIGHT);
+		} else {
+			width = viewerWidth;
+			height = getHeightFromWidth(width, ORIGINAL_WIDTH, ORIGINAL_HEIGHT);
+		}
+
+		left = (getScreenWidth() - width) / 2;
+		top = (viewerHeight - height) / 2;
+	} else {
+		width = getWidthFromHeight(height, ORIGINAL_WIDTH, ORIGINAL_HEIGHT);
+	}
+
+	return {
+		height,
+		left,
+		top,
+		width,
+	};
+};
--- src/TattooCanvas/TattooCanvas.tsx
+++ src/TattooCanvas/TattooCanvas.tsx
@@ -1,49 +1,24 @@
 import React, { useContext } from 'react';
-import { getHeightFromWidth, getWidthFromHeight } from '~helpers/numbers';
-import { getScreenWidth, getIsLandscape, getIsPortrait } from '~helpers/screen';
+import { getScreenWidth, getIsLandscape } from '~helpers/screen';
 import ImgTattooSmall from '~images/tattoo-small.jpg';
-import {
-	LAPTOP_HEIGHT,
-	LAPTOP_WIDTH,
-	ORIGINAL_HEIGHT,
-	ORIGINAL_WIDTH,
-	SPOTS,
-} from '../const';
+import { LAPTOP_HEIGHT, SPOTS } from '../const';
 import { Spot } from './Spot';
 import { MagnifyingGlass } from './MagnifyingGlass';
 import './TattooCanvas.scss';
 import { MobileContext } from 'src/App.context';
+import { getCanvasDimensions } from './TattooCanvas.helpers';
 
 const isLandscape = getIsLandscape();
-const isPortrait = getIsPortrait();
 const screenWidth = getScreenWidth();
 
 export const TattooCanvas = () => {
 	const { viewerHeight } = useContext(MobileContext);
 	const viewerWidth = isLandscape ? LAPTOP_HEIGHT : screenWidth;
 
-	let width: number;
-	let height = LAPTOP_HEIGHT;
-	let left = 0;
-	let top = 0;
-
-	if (isPortrait) {
-		const shouldUseHeight =
-			viewerWidth / viewerHeight > ORIGINAL_WIDTH / ORIGINAL_HEIGHT;
-
-		if (shouldUseHeight) {
-			height = viewerHeight;
-			width = getWidthFromHeight(height, ORIGINAL_WIDTH, ORIGINAL_HEIGHT);
-		} else {
-			width = viewerWidth;
-			height = getHeightFromWidth(width, ORIGINAL_WIDTH, ORIGINAL_HEIGHT);
-		}
-
-		left = (getScreenWidth() - width) / 2;
-		top = (viewerHeight - height) / 2;
-	} else {
-		width = getWidthFromHeight(height, ORIGINAL_WIDTH, ORIGINAL_HEIGHT);
-	}
+	const { height, left, top, width } = getCanvasDimensions(
+		viewerWidth,
+		viewerHeight,
+	);
 
 	return (
 		<div className="tattoo-canvas" style={{ height, left, top, width }}>
