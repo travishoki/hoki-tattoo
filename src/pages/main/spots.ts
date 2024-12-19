bcc1a87f581746fba9d6c9bafc658a1c0ad99ccc
Travis Hoki <travis.hoki@gmail.com>
2024-12-19 01:31:39 -0700

    figure spots

--- src/pages/main/spots.ts
+++ src/pages/main/spots.ts
@@ -3,15 +3,15 @@ import { SpotType } from 'src/types';
 // Tattoo Spots
 export const SPOTS: SpotType[] = [
 	{
-		link: '/dragon',
-		title: 'Dragon',
-		x: 0.2,
-		y: 0.2,
-	},
-	{
 		link: '/koi',
 		title: 'Koi',
-		x: 0.8,
-		y: 0.8,
+		x: 0.35,
+		y: 0.37,
+	},
+	{
+		link: '/dragon',
+		title: 'Dragon',
+		x: 0.65,
+		y: 0.37,
 	},
 ];
