e436e0d86faf8185c8d6bccc8b4fe274eb84de7d
Travis Hoki <travis.hoki@gmail.com>
2024-12-19 01:15:51 -0700

    homepage spots

--- src/pages/main/HomePage.tsx
+++ src/pages/main/HomePage.tsx
@@ -1,16 +1,21 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import { Page } from '../../components/Page/Page';
+import { SubPageLinks } from '~components/SubPageLinks/SubPageLinks';
+import { SPOTS } from './spots';
+
+const numberedSpots = SPOTS.map((spot, index) => ({
+	link: spot.link,
+	title: `${index + 1}) ${spot.title}`,
+}));
+
+const links = [...numberedSpots];
 
 export const HomePage = () => {
 	return (
-		<Page title="HomePage">
-			<Link title="Dragon" to="/dragon">
-				Dragon
-			</Link>
-			<Link title="Koi" to="/koi">
-				Koi
-			</Link>
+		<Page title="Pick A Spot">
+			<p>Click a circle to learn more</p>
+
+			<SubPageLinks links={links} />
 		</Page>
 	);
 };
---
+++ src/pages/main/spots.ts
@@ -0,0 +1,17 @@
+import { SpotType } from 'src/types';
+
+// Tattoo Spots
+export const SPOTS: SpotType[] = [
+	{
+		link: '/dragon',
+		title: 'Dragon',
+		x: 0.25,
+		y: 0.5,
+	},
+	{
+		link: '/koi',
+		title: 'Koi',
+		x: 0.75,
+		y: 0.5,
+	},
+];
