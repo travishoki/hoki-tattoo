2306b71623cfcd5af5abfdb308d257f2cc6d934b
Travis Hoki <travis.hoki@gmail.com>
2024-12-19 00:35:53 -0700

    spots

--- src/pages/dragon/HomePage.tsx
+++ src/pages/dragon/HomePage.tsx
@@ -22,6 +22,36 @@ const links = [
 		link: '/dragon/why',
 		title: 'Why',
 	},
+	{
+		link: '/dragon/koi',
+		title: 'Koi',
+		x: 0.63,
+		y: 0.725,
+	},
+	{
+		link: '/dragon/color',
+		title: 'Color',
+		x: 0,
+		y: 0,
+	},
+	{
+		link: '/dragon/japanese-vs-chinese',
+		title: 'Japanese vs Chinese',
+		x: 0,
+		y: 0,
+	},
+	{
+		link: '/dragon/physiology',
+		title: 'Physiology',
+		x: 0,
+		y: 0,
+	},
+	{
+		link: '/dragon//dragon/asian-vs-european',
+		title: 'Asian vs European',
+		x: 0,
+		y: 0,
+	},
 ];
 
 export const HomePage = () => {
