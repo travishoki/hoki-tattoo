ec403c40c4b0cdc8f6476b3f7e4f1723f93efe10
Travis Hoki <travis.hoki@gmail.com>
2024-12-20 18:22:09 -0700

    melody image

--- src/pages/melody/MeaningPage.tsx
+++ src/pages/melody/MeaningPage.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import { Page } from '../../components/Page/Page';
+import ImgMelody from '~images/melody-at-wedding.jpg';
+import ImgMelodySmall from '~images/melody-at-wedding-small.jpg';
+import { ImageMaximizable } from '~components/ImageModal/ImageMaximizable';
 
 export const MeaningPage = () => {
 	return (
 		<Page title="Meaning">
 			<p>Meaning</p>
+			<ImageMaximizable
+				alt="Melody"
+				dimensions={[300, 449]}
+				src={ImgMelodySmall}
+				srcLarge={ImgMelody}
+				srcLargeDimensions={[1367, 2048]}
+			/>
 		</Page>
 	);
 };
